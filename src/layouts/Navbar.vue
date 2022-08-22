88aecd36996d3979580bc664d7615755d5dd2b81
nutchayaporn-b <nutchayaporn_b@cmu.ac.th>
2022-08-23 02:31:53 +0700

    Setup project with css libs

---
+++ src/layouts/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <nav class="flex justify-between sticky top-0 px-4 py-4 bg-[#54BAB9] items-center">
+    <div class="text-4xl font-bold text-white">Vaccine</div>
+    <ul class="flex gap-12 text-white text-xl">
+      <li>Vaccination History</li>
+      <li>Docter Recommendation</li>
+      <li>Write Recommendation</li>
+    </ul>
+    <div class="text-white">
+      <i class="fas fa-user-circle text-3xl"></i>
+    </div>
+  </nav>
+</template>
