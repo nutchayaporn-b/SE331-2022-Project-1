7e3ea863e12235ccb7972fa2647db0d105167585
nutchayaporn-b <nutchayaporn_b@cmu.ac.th>
2022-08-23 22:58:48 +0700

    Setup pagination sysatem

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col w-[60%]">
-    <div class="grid grid-cols-3 gap-8">
+    <div class="md:grid md:grid-cols-3 gap-8">
       <!-- Render user card if user has been vaccinated -->
-      <template v-for="user in users">
+      <template v-for="(user, i) in users">
         <UserCard
-          v-if="user.vaccines.length > 0"
+          v-if="user.vaccines.length > 0 && i < currentPage * 6 + 1"
           :name="user.name"
           :surname="user.surname"
           :age="user.age"
@@ -15,19 +15,23 @@
         />
       </template>
     </div>
+    <Pagination :start="(users.length / currentPage) * 6" />
   </div>
 </template>
 <script>
 import UserCard from "../components/UserCard.vue";
 import users from "../mockups/users";
 import router from "../router";
+import Pagination from "../components/Pagination.vue";
 export default {
   components: {
     UserCard,
+    Pagination,
   },
   data() {
     return {
       users,
+      currentPage: 1,
     };
   },
   methods: {
