a08c0f9b9092499a136d8c3432dcec4ba30b4d73
nutchayaporn-b <nutchayaporn_b@cmu.ac.th>
2022-08-23 21:34:14 +0700

    minor changes

--- src/views/VaccineHistory.vue
+++ src/views/VaccineHistory.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="flex w-[60%] gap-4">
-    <UserCard
-      :name="user.name"
-      :surname="user.surname"
-      :age="user.age"
-      :hometown="user.hometown"
-      :dose="user.vaccines.length"
-    />
-    <table class="max-h-fit">
-      <tr class="text-black">
-        <th class="border-solid border-blue-500 border-4 px-4 py-1">Dose (#)</th>
-        <th class="border-solid border-blue-500 border-4 px-4 py-1">Date Of Vaccination</th>
-        <th class="border-solid border-blue-500 border-4 px-4 py-1">Vaccine ID</th>
-        <th class="border-solid border-blue-500 border-4 px-4 py-1">Name of Vaccination</th>
-      </tr>
-      <VaccineTableBodyInfo
-        v-for="vaccine in user.vaccines"
-        :dose="user.vaccines.indexOf(vaccine) + 1"
-        :vaccineDate="vaccine.timestamp"
-        :vaccineId="vaccine.id"
-        :vaccineName="vaccine.name"
+  <div class="flex flex-col w-[60%]">
+    <div class="flex gap-4">
+      <UserCard
+        :name="user.name"
+        :surname="user.surname"
+        :age="user.age"
+        :hometown="user.hometown"
+        :dose="user.vaccines.length"
       />
-    </table>
+      <table class="">
+        <tr class="text-black">
+          <th class="border-solid border-blue-500 border-4 px-4 py-1">Dose (#)</th>
+          <th class="border-solid border-blue-500 border-4 px-4 py-1">Date Of Vaccination</th>
+          <th class="border-solid border-blue-500 border-4 px-4 py-1">Vaccine ID</th>
+          <th class="border-solid border-blue-500 border-4 px-4 py-1">Name of Vaccination</th>
+        </tr>
+        <VaccineTableBodyInfo
+          v-for="vaccine in user.vaccines"
+          :dose="user.vaccines.indexOf(vaccine) + 1"
+          :vaccineDate="vaccine.timestamp"
+          :vaccineId="vaccine.id"
+          :vaccineName="vaccine.name"
+        />
+      </table>
+    </div>
+    <div class=""></div>
   </div>
 </template>
 <script>
